e20306034ce8e12201153fb45817c62a39f3bfb3
Anikait Panigrahi <anikait05@gmail.com>
2026-02-16 06:25:50 +0530

    [!] Update domain name

--- src/consts.ts
+++ src/consts.ts
@@ -4,7 +4,7 @@ export const SITE: Site = {
   title: 'br34d',
   description:
     'Electronics tinkerer and a hardware security enthusiast from India.',
-  href: 'https://anikait.space',
+  href: 'https://br34dcrumb.github.io/',
   author: 'br34dcrumb',
   locale: 'en-US',
   featuredPostCount: 0,
